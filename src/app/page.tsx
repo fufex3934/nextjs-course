f3c213c768e7a33e8719916688e839b250969238
fufex3934 <fufawakgari174@gmail.com>
2024-11-29 15:52:43 +0300

    routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
 
+'use client';
+import Link from "next/link"
+import { useRouter } from "next/navigation";
 const Home = () => {
+  const router = useRouter();
   return (
-    <div>Home</div>
+    <div>
+      <h1>Home</h1>
+      <Link href='/dashboard'>Dashboard</Link>
+      <button onClick={()=>router.push('/dashboard')}>Dashboard</button>
+    </div>
   )
 }
 
